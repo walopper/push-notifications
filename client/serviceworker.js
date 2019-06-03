2c5fa9243e2c45afbdc47b139904fdfda4dd5459
Alejandro Walo <walo.walo@gmail.com>
2019-06-03 20:34:24 -0300

    Update serviceworker.js

--- client/serviceworker.js
+++ client/serviceworker.js
@@ -1,45 +1,41 @@
 
 self.addEventListener('install', function (event) {
-    console.log('[Service Worker] Instalando ...');
+    // ...
 });
 
 self.addEventListener('activate', function (event) {
-    console.log('[Service Worker] Activando ....');
+    // ...
 });
 
-// escucho push notifications
-self.addEventListener('push', event => {
-    console.log('Push Notification recibida', event);
-
+self.addEventListener('push', function (event) {
     if (event.data) {
-        var pushdata = JSON.parse(event.data.json()); // ojo que no se define con let
+        var pushdata = JSON.parse(event.data.json());
     } else {
-        console.log('La notificacion vino sin data');
         return;
     }
 
     // ver opciones en https://developer.mozilla.org/es/docs/Web/API/ServiceWorkerRegistration/showNotification
-    const pushOptions = {
+    var pushOptions = {
         title: 'hola',
         body: pushdata.body,
         icon: '/images/badge.png',
-        // image: '/src/images/sf-boat.jpg', // es posible eniar una imagen de preview
+        image: '/src/images/sf-boat.jpg', // es posible eniar una imagen de preview
         dir: 'ltr',
         lang: 'es-ES', // BCP 47,
         vibrate: [100, 50, 200], // hago vibrar el movil con este patron. los tiempos son en milisegundos
-        // badge: '/src/images/icons/app-icon-96x96.png',
+        badge: '/src/images/icons/app-icon-96x96.png',
         tag: 'confirm-notification',
         renotify: true,
         actions: [
             {
                 action: 'confirm',
                 title: 'Okay',
-                // icon: '/src/images/icons/app-icon-96x96.png' 
+                icon: '/src/images/icons/app-icon-96x96.png' 
             },
             {
                 action: 'cancel',
                 title: 'Cancel',
-                // icon: '/src/images/icons/app-icon-96x96.png' 
+                icon: '/src/images/icons/app-icon-96x96.png' 
             }
         ],
         // puedo abrir una URL al hacer click en la tarjeta de notificacion
@@ -49,6 +45,6 @@ self.addEventListener('push', event => {
     }
 
     event.waitUntil(
-        self.registration.showNotification(pushdata.title, pushOptions) // => promesa
+        self.registration.showNotification(pushdata.title, pushOptions) // => promise
     );
 });
